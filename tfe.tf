cf46646afcf8d80ec901ae668ee9df54982e20bd
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2022-08-25 12:35:12 +0200

    Update tfe.tf

--- tfe.tf
+++ tfe.tf
@@ -1,6 +1,13 @@
 locals {
   tfe_org = try(data.tfe_organization.orgs[0], {})
   tfe_workspaces = try(data.tfe_organization.workspaces.ids, {})
+  tfe_workspace_outputs = {
+    for workspace_name, workspace_outputs in data.tfe_outputs.workspace_outputs :
+        workspace_name => {
+          for output_name, output_value in workspace_outputs.values :
+              output_name => output_value
+        }
+  }
 }
 
 # Find Terraform Organization
@@ -17,4 +24,11 @@ data "tfe_workspace_ids" "workspaces" {
   organization = local.tfe_org.name
   
   names = [*]
+}
+
+data "tfe_outputs" "workspace_outputs" {
+  for_each = data.tfe_workspace_ids.workspaces.full_names
+  
+  organization = local.tfe_org
+  name = each.key
 }
