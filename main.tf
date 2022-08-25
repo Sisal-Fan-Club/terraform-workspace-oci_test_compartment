4db0a275156c1e3e1c58348dc57291ad8f40ff61
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2022-08-25 12:27:04 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,27 +18,6 @@ provider "tfe" {
   token = var.tfe_org_token
 }
 
-locals {
-  tfe_org = try(data.tfe_organization.orgs[0], {})
-  tfe_workspaces = try(data.tfe_organization.workspaces.ids, {})
-}
-
-# Find Terraform Organization
-data "tfe_organizations" "orgs" {
-}
-
-data "tfe_organization" "orgs" {
-  for_each = data.tfe_organizations.orgs.ids
-  
-  name = each.value
-}
-
-data "tfe_workspace_ids" "workspaces" {
-  organization = local.tfe_org.name
-  
-  names = [*]
-}
-
 # OCI regional provider
 provider "oci" {
   tenancy_ocid = var.oci_tenancy_id
